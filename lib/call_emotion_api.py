c9f79d2ab42e82f83478ed6d66b864dcc05568ec
ryotarogoto <fifty.fiftyryo0401.0613@gmail.com>
2013-12-09 22:24:03 +0900

    修正

--- lib/call_emotion_api.py
+++ lib/call_emotion_api.py
@@ -5,6 +5,10 @@ import sys
 import requests
 import json
 
+dislike = "生は永久の闘いである。 自然との闘い、社会との闘い、他との闘い、永久に解決のない闘いである。 闘え。闘いは生の花である。"
+fear = "人生は恐れなければ、とても素晴らしいものなんだよ。人生に必要なもの。それは勇気と想像力、そして少しのお金だ。"
+sad = "何でも謝って済むことではないけれど 謝れない人間は最低だ"
+
 text = sys.argv[1]
 url = "http://ap.mextractr.net/ma9/emotion_analyzer"
 query = {
@@ -17,24 +21,15 @@ res = requests.get("http://ap.mextractr.net/ma9/emotion_analyzer",
     params=query)
 
 data = json.loads(res.text)
-print(data)
+rank_like = data["likedislike"]
+rank_anger = data["angerfear"]
+rank_joy = data["joysad"]
 
-print("likedislike = %s  angerfear = %s  joysad = %s" 
-        % (data["likedislike"], data["angerfear"], data["joysad"]))
-
-dislike = "生は永久の闘いである。 自然との闘い、社会との闘い、他との闘い、永久に解決のない闘いである。 闘え。闘いは生の花である。"
-fear = "人生は恐れなければ、とても素晴らしいものなんだよ。人生に必要なもの。それは勇気と想像力、そして少しのお金だ。"
-sad = "何でも謝って済むことではないけれど 謝れない人間は最低だ"
-
-result = ""
-if(data["likedislike"] >= data["angerfear"]):
+if(rank_like <= rank_anger and rank_like <= rank_joy):
     result = dislike
-if(data["likedislike"] >= data["joysad"]):
-    result = dislike
+elif(rank_anger <= rank_like and rank_joy <= rank_like):
+    result = fear
 else:
-    if(data["angerfear"] >= data["joysad"]):
-        result = fear
-    else:
-        result = sad
+    result = sad
 
 print("あなたにピッタリの名言は : %s" % result)
